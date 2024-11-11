65845c793c7788d5888723495d4e9fd053e0e5e0
KristaNorth <165333028+KristaNorth@users.noreply.github.com>
2024-11-11 17:47:09 -0600

    Added first draft of Javascript to project

---
+++ javascript/index.js
@@ -0,0 +1,27 @@
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss: [(<small>]A[</small>)]"
+  );
+
+  //Toronto
+  let toronto = document.querySelector("#toronto");
+  let torontoDateElement = torontoElement.querySelector(".date");
+  let torontoTimeElement = torontoElement.querySelector(".time");
+  let torontoTime = moment().tz("America/Toronto");
+
+  torontoDateElement.innerHTML = torontoTime.format("MMMM Do YYYY");
+
+  torontoTimeElement.innerHTML = torontoTime.format(
+    "h:mm:ss: [(<small>]A[</small>)]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
